978896ecc7e437b704577b4e78fdcde411258dd6
rede97 <m454078356@163.com>
2025-03-12 17:02:12 +0800

    init env

--- init_env.sh
+++ init_env.sh
@@ -1,5 +1,4 @@
 #!/bin/env sh
 conda init
 conda create -n yolov5 python=3.10
-pip install -r 
 pip install -r requirements.txt
--- train_yolov5n_face.sh
+++ train_yolov5n_face.sh
@@ -1,2 +1,3 @@
+conda init
 conda activate yolov5
 nohup python train.py --cfg models/yolov5n.yaml --weights weights/yolov5n-face.pt --epochs 50 &
